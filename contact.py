81e9c8de7aa9ca328ef94c08f458ea12d31069a7
alexng1991 <alexng1991@gmail.com>
2017-12-05 16:57:15 -0600

    changed to use gmail

--- contact.py
+++ contact.py
@@ -2,22 +2,25 @@
 import smtplib
 
 # Import the email modules we'll need
-from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
+#login to gmail
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("haccingprotecctor@gmail.com", "ishallprotecc")
 # Open a plain text file for reading.  For this example, assume that
 # the text file contains only ASCII characters.
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+textfile = open(os.path.join(__location__, 'attackers.txt'));
+
+msg = ""
+
 with open(textfile) as fp:
     # Create a text/plain message
     msg = MIMEText(fp.read())
 
-# me == the sender's email address
-# you == the recipient's email address
-# textfile = depending of situation
-msg['Subject'] = 'ALERT: %s' % textfile
-msg['From'] = me
-msg['To'] = you
-
-# Send the message via our own SMTP server.
-s = smtplib.SMTP('localhost')
-s.send_message(msg)
-s.quit()
+#for now, sending email to myself
+server.sendmail("haccingprotecctor@gmail.com", "haccingprotecctor@gmail.com", msg)
+server.quit()
