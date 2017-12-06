0eb212c963135b007d92b2a69b687169bb65a5ad
Alex <alexng1991@gmail.com>
2017-12-05 18:46:01 -0600

    email sends successfully attacker.txt

--- contact.py
+++ contact.py
@@ -1,9 +1,10 @@
 # Import smtplib for the actual sending function
 import smtplib
-
+import os
+import sys
 # Import the email modules we'll need
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 #login to gmail
 server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -11,10 +12,7 @@ server.starttls()
 server.login("haccingprotecctor@gmail.com", "ishallprotecc")
 # Open a plain text file for reading.  For this example, assume that
 # the text file contains only ASCII characters.
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-textfile = open(os.path.join(__location__, 'attackers.txt'));
-
+textfile = os.path.join(sys.path[0], 'attackers.txt')
 msg = ""
 
 with open(textfile) as fp:
@@ -22,5 +20,5 @@ with open(textfile) as fp:
     msg = MIMEText(fp.read())
 
 #for now, sending email to myself
-server.sendmail("haccingprotecctor@gmail.com", "haccingprotecctor@gmail.com", msg)
+server.sendmail("haccingprotecctor@gmail.com", "haccingprotecctor@gmail.com", msg.as_string())
 server.quit()
